d914ce6557bebc87ce6e826c3f5e39a46457127b
xxyeon129 <xxyeon129@gmail.com>
2023-05-12 21:18:32 +0900

    Chore: initial setting

---
+++ src/GlobalStyle.js
@@ -0,0 +1,7 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    /* 전역 스타일 */
+`;
+
+export default GlobalStyle;
---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const PATH_URL = {
+  MAIN: '/',
+  LOGIN: '/user/login',
+  SIGNUP: '/user/signup',
+  DETAIL: '/detail/:detailId',
+};
