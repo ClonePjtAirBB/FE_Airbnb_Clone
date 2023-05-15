df90ec6330aec87368ef2dd6cc1625b6952807ab
xxyeon129 <xxyeon129@gmail.com>
2023-05-15 14:40:15 +0900

    Test: 서버 요청 테스트 코드

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,15 @@ import axios from 'axios';
 
 const Login = () => {
   const url = 'http://43.200.169.48';
-  axios.get(`${url}/api/stay`).then(res => console.log(res));
+  // axios.get(`${url}/api/stay`).then(res => console.log(res));
+
+  axios
+    .post(`${url}/api/user/signup`, {
+      email: 'test123@gmail.com',
+      password: 'test12345',
+      nickname: 'test12345',
+    })
+    .then(res => console.log(res));
 
   return <>login</>;
 };
