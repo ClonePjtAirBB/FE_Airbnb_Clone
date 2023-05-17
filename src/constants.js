dbe9ff089bb96a15ab7af7ea8d131d7919f6ec85
xxyeon129 <xxyeon129@gmail.com>
2023-05-18 04:46:20 +0900

    Style: 사용하지 않는 Router 경로 삭제

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,5 @@
 export const PATH_URL = {
   MAIN: '/',
-  LOGIN: '/user/login',
-  SIGNUP: '/user/signup',
   PATH_DETAIL: '/detail/:id',
   DETAIL: '/detail',
 };
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from '../pages/Main';
 import { PATH_URL } from '../constants';
-import Login from '../pages/Login';
 import Signin from '../pages/Signin';
 import Detail from '../pages/Detail';
 import Layout from './Layout';
@@ -12,7 +11,6 @@ const Router = () => {
       <Layout>
         <Routes>
           <Route path={PATH_URL.MAIN} element={<Main />} />
-          <Route path={PATH_URL.LOGIN} element={<Login />} />
           <Route path={PATH_URL.SIGNUP} element={<Signin />} />
           <Route path={PATH_URL.PATH_DETAIL} element={<Detail />} />
         </Routes>
